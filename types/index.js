a5bc265f88cd6c50d027c91e60189e139cbed20a
José Carvalho <jcarvalhoprojects@gmail.com>
2020-02-26 13:52:18 +0100

    Add initial exercises for types

---
+++ types/index.js
@@ -0,0 +1,42 @@
+/*
+    Strings
+
+    Exercise 1: print a string
+    Exercise 2: Concatenate two strings (e.g. "Hello" and "world" and print "Hello world")
+    Exercise 3: Print the number of characters in a string (e.g. "Hello" has 5 characters)
+    Exercise 4: Print the character set in a specific position of a string (e.g. "Hello world", print "w")
+    Exercise 5: Print a part of a string (e.g. "Hello world", print "world")
+    Exercise 6: Replace a section of the text (e.g. "Hello world", print "Hello universe")
+    Exercise 7: Lower case some text (e.g. "Hello World", print "hello world")
+    Exercise 8: Upper case some text (e.g. "Hello World", print "HELLO WORLD")
+    Exercise 9: Remove the white spaces after the text (e.g. "Hello world      ", print "Hello world")
+    Exercise 10: Remove the white spaces before the text (e.g. "       Hello world", print "Hello world")
+    Exercise 11: Remove the white spaces before and after the text (e.g. "       Hello world        ", print "Hello world")
+    Exercise 12: Convert a number to a string representation (e.g. 4 to "4")
+*/
+
+console.log('----------- STRINGS -----------\n\n\n');
+
+/*
+    Numbers
+
+    Exercise 1: print a number
+    Exercise 2: Add two numbers (e.g. add 1 and 2, print 3)
+    Exercise 3: Subtract two numbers (e.g. subtract 2 and 1, print 1)
+    Exercise 4: Multiply two numbers (e.g. multiply 4 and 2, print 8)
+    Exercise 5: Divide two numbers (e.g. divide 4 and 2, print 2)
+    Exercise 6: Convert a string to an integer (e.g. "4" to 4)
+    Exercise 7: Convert a string to a float (e.g. "4.4" to 4.4)
+    Exercise 8: Convert a floating number to a fixed precision (e.g convert 123.456 to 123.46)
+*/
+
+console.log('----------- NUMBERS -----------\n\n\n');
+
+/*
+    Booleans
+
+    Well a boolean is basically true or false
+    More on that later when we discuss operators and conditionals
+*/
+
+console.log('----------- BOOLEANS -----------\n\n\n');
