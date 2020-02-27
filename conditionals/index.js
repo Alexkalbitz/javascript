579f903f16b39c5a0ee4b1b06828e6dc1839ed88
José Carvalho <jcarvalhoprojects@gmail.com>
2020-02-27 10:13:23 +0100

    Add conditionals hands on

---
+++ conditionals/index.js
@@ -0,0 +1,32 @@
+/*
+    if statement
+    
+    Exercise 1: Check if value is even and print "even number" or "odd number" otherwise
+    Exercise 2: Check if value is between 50 and 100 and print either "Between 50 and 100" or "Between 0 and 49" otherwise
+    Exercise 3: Check if value is between 50 and 100 or if its an odd number and print either "Between 50 and 100 or odd". Else print "No match"
+    Exercise 4: Check if value is between 50 and 100 and print either "Between 50 and 100", else check if value is odd and print "odd number".
+*/
+const value = Math.floor(Math.random() * 100);
+
+/*
+    switch statement
+    
+    Print these texts if direction matches a value
+
+    direction is 0 -> "Go forward"
+    direction is 1 -> "Go right"
+    direction is 2 -> "Go backwards"
+    direction is 3 -> "Go left"
+    else -> "stay"
+*/
+const direction = Math.round(Math.random() * 3)
+
+/*
+    ternary operator
+
+    Exercise 1: If value is between 50 and 100, print "between 50 and 100" else print "between 0 and 49"
+    Exercise 2: If value is an even number, print the number divide by 2 else print the value multiplied by 2
+*/
+
+
+
