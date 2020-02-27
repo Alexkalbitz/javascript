ce71de4ebf151be68f7dece798571c5d94dc916f
José Carvalho <jcarvalhoprojects@gmail.com>
2020-02-27 10:35:06 +0100

    Add functions assignment

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+/*
+    Exercise 1: Create a function returns the sum of 2 numbers (e.g. sum (1,2) returns 3)
+    Exercise 2: Create a function that multiplies 2 numbers (e.g. multiply (2,3) returns 6)
+    Exercise 3: Create a function that divides 2 numbers (e.g. divide (6,3) returns 2)
+    Exercise 4: Create a function that checks if value is a odd number (e.g. isOdd(3) returns true)
+    Exercise 5: Create a function that halves a number if its even or multiplies the number by 2 if its odd, using the functions from the previous exercises (e.g. halveOrDouble(3) returns 6, halveOrDouble(4) returns 2)
+*/
+
+
+
