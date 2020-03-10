345036bdf863834f2256face818416ef0db9b1c5
José Carvalho <jcarvalhoprojects@gmail.com>
2020-03-10 15:48:41 +0100

    Add loops exercises

---
+++ loops/index.js
@@ -0,0 +1,32 @@
+/*
+    Exercise 1: Use a for loop to print all the even numbers between 0 and 5
+    
+    Exercise 2: Given a size of a table/matrix, use a nested loop to print to print each cell as the result of the sum between the row and the column index
+    (e.g.)
+        for(...) {
+            for(...) {
+                console.log(...)
+            }
+        }
+        
+        Table/matrix size 3 should print
+            - 'row 0 column 0: 0'
+            - 'row 0 column 1: 1'
+            - 'row 0 column 2: 2'
+            - 'row 1 column 0: 1'
+            - 'row 1 column 1: 2'
+            - 'row 1 column 2: 3'
+            - 'row 2 column 0: 2'
+            - 'row 2 column 1: 3'
+            - 'row 2 column 2: 4'    
+    
+    Exercise 3: Exercise 1 using a while loop
+
+    Exercise 4: Exercise 2 using a nested while loop
+    (e.g.)
+        while(...) {
+            while(...)
+        }
+
+    Exercise 5: Exercise 4 using a single while loop
+*/
