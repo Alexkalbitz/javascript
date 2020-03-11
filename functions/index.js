5888053f272bc7f94b0c96649c2b61bfa74cb968
José Carvalho <jcarvalhoprojects@gmail.com>
2020-03-11 09:12:32 +0100

    Add functions solutions

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,37 @@
     Exercise 5: Create a function that halves a number if its even or multiplies the number by 2 if its odd, using the functions from the previous exercises (e.g. halveOrDouble(3) returns 6, halveOrDouble(4) returns 2)
 */
 
+function sum(a, b) {
+    return a + b;
+}
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function isOdd(a) {
+    return a % 2 !== 0;
+}
+
+function isEven(a) {
+    return !isOdd(a);
+}
+
+function halveOrDouble(a) {
+    return isOdd(a) ? multiply(a, 2) : divide(a, 2);
+}
+
+console.log('sum:', sum(1, 2));
+console.log('multiply:', multiply(4, 2));
+console.log('divide:', divide(4, 2));
+console.log('isOdd:', isOdd(3));
+console.log('isOdd:', isOdd(4));
+console.log('isEven:', isEven(2));
+console.log('isEven:', isEven(3));
+console.log('halveOrDouble:', halveOrDouble(4));
+console.log('halveOrDouble:', halveOrDouble(3));
 
